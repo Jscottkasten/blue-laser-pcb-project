373bac30f3ce09b9119b4186dd0063949379dbdd
J. Scott Kasten <jsk@yahoo.com>
2017-08-07 20:34:51 -0400

    Convert to GIT.

---
+++ gaf/laser-const-current-pwr/backup3/board.cmd
@@ -0,0 +1,59 @@
+# Pin name action command file
+
+# Start of element R5
+ChangePinName(R5, 1, 1)
+ChangePinName(R5, 2, 2)
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
+
+# Start of element LD1
+ChangePinName(LD1, 2, ANODE)
+ChangePinName(LD1, 1, CATHODE)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 3, 3)
+ChangePinName(R2, 2, 2)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element C3
+ChangePinName(C3, N, -)
+ChangePinName(C3, P, +)
+
+# Start of element B1
+ChangePinName(B1, 2, -)
+ChangePinName(B1, 1, +)
+
+# Start of element C2
+ChangePinName(C2, 2, 2)
+ChangePinName(C2, 1, 1)
+
+# Start of element S1
+ChangePinName(S1, 2, 2)
+ChangePinName(S1, 1, 1)
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element D1
+ChangePinName(D1, 2, 2)
+ChangePinName(D1, 1, 1)
+
+# Start of element U1
+ChangePinName(U1, 1, Adjust)
+ChangePinName(U1, 3, Vin)
+ChangePinName(U1, 2, Vout)
+
+# Start of element J1
+ChangePinName(J1, 1, 1)
+ChangePinName(J1, 2, 2)
